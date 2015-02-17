6aba438a6db2f006ad5856c650e3f313e8be59fc
Carlos J. Rosales <cjrosales@gmail.com>
2015-02-17 08:53:48 +0100

    Adjusting the startRecording script, and fix typo in uploadSetup

--- scenario1/data/startRecording.sh
+++ scenario1/data/startRecording.sh
@@ -2,32 +2,37 @@
 
 clear
 
-echo "Type experiment name: "
+echo "Type experiment name (use only letters and one single string of characters): "
 read folder
 echo "A folder named $folder will contain all data related to your experiment"
 mkdir $folder
 cd $folder
 
-echo "Recording will start in 2 seconds. After that you will receive the signal to start moving"
+echo "Recording will start in 5 seconds. After that you will receive the signal to start moving"
 
 echo "Saving experiment parameters..."
 rosparam dump $folder.yaml
 
-sleep 1
-
 echo "Starting recording process, remember it will only record for 8 seconds..."
+
+sleep 5
 gnome-terminal --disable-factory --title="RECORDING" -x bash -c "rosbag record -O $folder.bag --duration=8 /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
 PID=$!
 
-
-sleep 1
+sleep 2
 
 echo "----------------------- GO! ---------------------------------"
 
-sleep 10
+sleep 8
+
+kill -9 $PID
 
 echo "----------------------- EXPERIMENT IS OVER ------------------"
-kill -9 $PID
+
+sleep 3
+
 
 echo "A bag file was generated with sensor data, check the readme the instructions to play it back."
 echo "Farewell"
+
+sleep 3
