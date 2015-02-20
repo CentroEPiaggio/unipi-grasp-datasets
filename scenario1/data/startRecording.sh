f1dc9632db5e75076566ddcecb1c9a0641e573a5
Federico Spinelli <fspinelli@gmail.com>
2015-02-20 11:45:36 +0100

    Restored Readme and reupdated startRecording to last 8s

--- scenario1/data/startRecording.sh
+++ scenario1/data/startRecording.sh
@@ -11,36 +11,21 @@ cd $folder
 echo "Saving experiment parameters..."
 rosparam dump $folder.yaml
 
-echo "====================================================================="
-echo "Type [ENTER] key when you are ready to start recording approach phase"
-echo "====================================================================="
+echo "============================================================================================"
+echo "Type [ENTER] key when you are ready to start recording (the record will last only 8 seconds)"
+echo "============================================================================================"
 read k </dev/tty
 
-gnome-terminal --disable-factory --title="RECORDING_APPROACH" -x bash -c "rosbag record -O ${folder}_APP.bag /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
+gnome-terminal --disable-factory --title="RECORDING" -x bash -c "rosbag record -O ${folder}.bag --duration=8 /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
 PID=$!
-echo "Recording approach..."
+echo "Recording..."
+
+sleep 9
 
 echo "============================================"
-echo "Type [ENTER] key when you are ready to grasp"
+echo "DONE! Record is finished"
 echo "============================================"
-read key </dev/tty
 kill -9 $PID
-
-echo "Recording grasp..."
-
-gnome-terminal --disable-factory --title="RECORDING_GRASP" -x bash -c "rosbag record -O ${folder}_GRP.bag /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
-PID=$!
-
-echo "================================================="
-echo "Type [ENTER] key to stop recording the experiment"
-echo "================================================="
-read key </dev/tty
-kill -9 $PID
-
-rosbag reindex ${folder}_*.bag.active -f -q
-mv ${folder}_APP.bag.active ${folder}_APP.bag
-mv ${folder}_GRP.bag.active ${folder}_GRP.bag
-rm ${folder}_APP.bag.orig.active ${folder}_GRP.bag.orig.active
 
 echo "Bag files were generated with sensor data, check the readme for instructions to play them back."
 echo "Farewell!"
