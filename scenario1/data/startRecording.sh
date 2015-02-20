374cd45146f5d2e0e31a8c1d9a85837e2d05733f
Federico Spinelli <fspinelli@gmail.com>
2015-02-20 18:06:06 +0100

    updated startRecording to record for 15s instead of 8

--- scenario1/data/startRecording.sh
+++ scenario1/data/startRecording.sh
@@ -12,19 +12,53 @@ echo "Saving experiment parameters..."
 rosparam dump $folder.yaml
 
 echo "============================================================================================"
-echo "Type [ENTER] key when you are ready to start recording (the record will last only 8 seconds)"
+echo "Type [ENTER] key when you are ready to start recording (the record will last only 15 seconds)"
 echo "============================================================================================"
 read k </dev/tty
+
+sleep 1
+echo "============================================================================================"
+echo "                                        GO!!!"
+echo "============================================================================================"
 
 gnome-terminal --disable-factory --title="RECORDING" -x bash -c "rosbag record -O ${folder}.bag --duration=8 /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
 PID=$!
 echo "Recording..."
+echo "15"
+sleep 1
+echo "14"
+sleep 1
+echo "13"
+sleep 1
+echo "12"
+sleep 1
+echo "11"
+sleep 1
+echo "10"
+sleep 1
+echo "9"
+sleep 1
+echo "8"
+sleep 1
+echo "7"
+sleep 1
+echo "6"
+sleep 1
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
 
-sleep 9
+echo "============================================================================================"
+echo "                      DONE Recording, Experiment is finished"
+echo "============================================================================================"
 
-echo "============================================"
-echo "DONE! Record is finished"
-echo "============================================"
 kill -9 $PID
 
 echo "Bag files were generated with sensor data, check the readme for instructions to play them back."
