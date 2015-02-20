2bb4ae7b46361973cc2dcf08af84cc3abd68a798
Carlos J. Rosales <cjrosales@gmail.com>
2015-02-20 18:36:00 +0100

    Update startRecording.sh

--- scenario1/data/startRecording.sh
+++ scenario1/data/startRecording.sh
@@ -11,17 +11,13 @@ cd $folder
 echo "Saving experiment parameters..."
 rosparam dump $folder.yaml
 
-echo "============================================================================================"
-echo "Type [ENTER] key when you are ready to start recording (the record will last only 15 seconds)"
-echo "============================================================================================"
-read k </dev/tty
-
 sleep 1
+
 echo "============================================================================================"
 echo "                                        GO!!!"
 echo "============================================================================================"
 
-gnome-terminal --disable-factory --title="RECORDING" -x bash -c "rosbag record -O ${folder}.bag --duration=8 /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
+gnome-terminal --disable-factory --title="RECORDING" -x bash -c "rosbag record -O ${folder}.bag --duration=15 /camera/depth_registered/points_drop phase_space_markers tf /flexiforce/raw_values; bash" &
 PID=$!
 echo "Recording..."
 echo "15"
