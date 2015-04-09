48d8d4b33cb4de30d2e590c110e7e9bebcdcf51d
Federico Spinelli <fspinelli@gmail.com>
2015-04-09 09:44:55 +0200

    Fixed previously wrong merging, oops!

--- pacman_wp2_grasp_db/data/startRecording.sh
+++ pacman_wp2_grasp_db/data/startRecording.sh
@@ -18,45 +18,13 @@ echo "==========================================================================
 echo "                                        GO!!!"
 echo "============================================================================================"
 
-gnome-terminal --disable-factory --title="RECORDING" -x bash -c "rosbag record -O ${folder}.bag --duration=15 /cloud_stream /video_stream phase_space_markers tf /flexiforce/raw_values; bash" &
-PID=$!
-echo "Recording..."
-echo "15"
-sleep 1
-echo "14"
-sleep 1
-echo "13"
-sleep 1
-echo "12"
-sleep 1
-echo "11"
-sleep 1
-echo "10"
-sleep 1
-echo "9"
-sleep 1
-echo "8"
-sleep 1
-echo "7"
-sleep 1
-echo "6"
-sleep 1
-echo "5"
-sleep 1
-echo "4"
-sleep 1
-echo "3"
-sleep 1
-echo "2"
-sleep 1
-echo "1"
-sleep 1
+rosbag record -O record.bag --duration=10 cloud_stream video_stream phase_space_markers tf /flexiforce/raw_values 
 
 echo "============================================================================================"
 echo "                      DONE Recording, Experiment is finished"
 echo "============================================================================================"
 
-echo "Bag files were generated with sensor data, check the readme for instructions to play them back."
+echo "Bag file 'record.bag'  was generated with sensor data, check the readme for instructions to play it back."
 echo "Farewell!"
 
 sleep 1
